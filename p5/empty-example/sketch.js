496b0fbd4f24035189f4b555e9ee38811a10cfb9
Havis <holly2.havis@live.uwe.ac.uk>
2021-11-25 11:21:29 +0000

    values added

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -5,6 +5,10 @@ function setup() {
   x = width/2;  //middle
   y = height/2; //centre
   size = 10;
+  xSpeed = random(0.3,5);
+  ySpeed = random(0.3,5);
+  xDir = 1;
+  yDir = 1;
 }
 
 function draw() {
